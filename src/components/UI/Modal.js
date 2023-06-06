3c050efad6248b515050834b56dede08e99c14ed
HemantWD <hemantajaysharma@gmail.com>
2023-06-06 12:02:08 +0530

    made modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+// creating a helper function because we want to know where to portal the element,to get access to that overlay in the index.html file.
+const portalElement = document.getElementById("overlays");
+
 const Modal = (props) => {
-  return <div></div>;
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
 };
 
 export default Modal;
